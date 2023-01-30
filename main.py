23a1afed6ca1222c98c8ef8bdbf245aff1bf01e2
MarkCrux <47409696+MarkCrux@users.noreply.github.com>
2023-01-31 01:44:28 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -34,63 +34,11 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/')
+
 def video_feed():
     return Response(gen(vc()), mimetype = 'multipart/x-mixed-replace; boundary=frame')
 
 
-
-# def gaze_img():
-#
-#     fname = './img/1.jpg'
-#     # img = cv2.imread(fname)
-#     return send_file(fname, mimetype='image/jpg')
-    # gaze = GazeEstimate()
-    # cap = cv2.VideoCapture(0)
-    #
-    # while cap.isOpened():
-    #     success, image = cap.read()
-    #     if not success:
-    #         print("Ignoring empty camera frame.")
-    #         # If loading a video, use 'break' instead of 'continue'.
-    #         continue
-    #     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    #     gaze.process(image)
-    #     pose_direction = gaze.pose_direction()
-    #     eye_ratio = gaze.eye_direction()
-    #
-    #     image = gaze.annotated_frame(pose_direction, eye_ratio)
-    #
-    #     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    #
-    #     # cv2.imshow('Gaze Estimation', image)
-    #     # if cv2.waitKey(50) & 0xFF == 27:
-    #     #     break
-    #     return(image)
-    # cap.release()
-
-
-# gaze = GazeEstimate()
-# cap = cv2.VideoCapture(0)
-#
-# while cap.isOpened():
-#     success, image = cap.read()
-#     if not success:
-#         print("Ignoring empty camera frame.")
-#         # If loading a video, use 'break' instead of 'continue'.
-#         continue
-#     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-#     gaze.process(image)
-#     pose_direction = gaze.pose_direction()
-#     eye_ratio = gaze.eye_direction()
-#
-#     image = gaze.annotated_frame(pose_direction, eye_ratio)
-#
-#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-#
-#     cv2.imshow('Gaze Estimation', image)
-#     if cv2.waitKey(50) & 0xFF == 27:
-#         break
-# cap.release()
 
 if __name__ == '__main__':
     app.run()
